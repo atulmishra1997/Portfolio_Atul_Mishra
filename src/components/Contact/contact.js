c2d296a71d91ad6676899e6726e6bfafaa58bfcf
Atul Mishra <atulmishra1997@hotmail.com>
2023-11-20 12:29:16 +0530

    1.0.1

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -32,10 +32,10 @@ const Contact = () => {
                     <textarea className="msg" name="message" rows="5" placeholder="Your Message"></textarea>
                     <button type="submit" value='Send' className="submitBtn">Submit</button>
                     <div className="links">
-                        <img src={FacebookIcon} alt="Facebook" className="link"/>
-                        <img src={TwitterIcon} alt="Twitter" className="link"/>
-                        <img src={YoutubeIcon} alt="Youtube" className="link"/>
-                        <img src={InstagramIcon} alt="Instagram" className="link"/>
+                        <a href="https://www.facebook.com/profile.php?id=100091441811251"><img src={FacebookIcon} alt="Facebook" className="link"/></a>
+                        <a href="https://twitter.com/brandys_vines1"><img src={TwitterIcon} alt="Twitter" className="link"/></a>
+                        <a href="https://www.youtube.com/channel/UC8HpyjHZaJJ4jN1w3XUakGg"><img src={YoutubeIcon} alt="Youtube" className="link"/></a>
+                        <a href="https://www.instagram.com/atul_mishra1997/"><img src={InstagramIcon} alt="Instagram" className="link"/></a>
                     </div>
                 </form>
             </div>
